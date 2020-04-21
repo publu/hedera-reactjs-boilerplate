35795b1182c69ff3e3260c3e3f6afe2bbbbca6ca
Pablo <pablo@hashingsystems.com>
2020-04-21 15:30:00 -0500

    fixed the hedera.ts import file

--- src/hedera.ts
+++ src/hedera.ts
@@ -1,83 +1,112 @@
-// This code is generated automatically using deployer's(console.hashingsystems.com) interface generator
-// @createdDate : Sun Apr 19 2020 23:27:38 GMT-0500 (Central Daylight Time) 
-// @dependencies : hash-sdk 
-// @language : TypeScript
-// hash-sdk should be installed 
-// # npm i hash-sdk 
-// # yarn add hash-sdk 
+import hash from "hash-sdk";
 
-import hash from "hash-sdk"; 
-
-const hashSdk = {...hash}
-
-   /**
-    * Initializes an account for interacting with below functions
+ 
+interface TopicUpdateData {
+    topicId: string;
+    memo?: string;
+    topicName?: string;
+    submitKeyList?: string|string[];
+    expirationTime?: string|number;
+  }
+  interface TopicInfoData {
+    topicId: string;
+    memo: string;
+  }
+  interface SubmitMessageData {
+    topicId: string;
+    memo: string;
+    message: string;
+  }
+ 
+ 
+  interface AccountDataLike {
+    accountId: string;
+    network: string;
+    keys?: {
+        privateKey: string;
+        publicKey?: string;
+    };
+    mnemonics?: string;
+}
+ 
+interface ContractFunctionCallData {
+    contractid: string;
+    memo: string;
+    abi: Array<any>;
+    params: Array<string|number|boolean>;
+    amount?: number;
+  }
+ 
+  interface FileRetrieveData {
+    fileId: string;
+    memo: string;
+  }
+export default {
+ 
+        /**
+    * nitializes an account for interacting with below functions
     */
-    interface IAccountDataLike {
-        accountId:string,
-        network:string,
-        keys?:{
-            privateKey:string
-            publicKey?:string
-        },
-        mnemonics?:string
-    }
- const init = async()=> {
+    init : async()=> {
         try{
             // Setting it default to software it talks to sdk directly
-            await hash.setProvider('software');
-            const accountData :IAccountDataLike= {
-                accountId: ''/*<accountId(0.0.1234)>*/,
-                network: ''/*<mainnet | testnet>*/,
+            await hash.setProvider("software");
+            const accountData: AccountDataLike= {
+                accountId: ""/*<accountId(0.0.1234)>*/,
+                network: ""/*<mainnet | testnet>*/,
                 keys:{
-                    privateKey:''/*<aplphanumeric user privatekey>*/
+                    privateKey:""/*<aplphanumeric user privatekey>*/
                 },
-                mnemonics:''/*<phrase>*/
-            }
+                mnemonics:""/*<phrase>*/
+            };
             await hash.setAccount(accountData);
         }catch(e){
-            console.log('Error in intializing account:::',e);
+            console.log("Error in intializing account:::",e);
             throw e;
         }
+    },
+ 
+scHelloWorld : {
+  update : async(newMessage: string) => {
+      const data: ContractFunctionCallData= {
+          memo: "smart contract call",
+          contractid: "0.0.28595",
+          abi:[{"constant":false,"inputs":[{"name":"newMessage","type":"string"}],"name":"update","payable":false,"stateMutability":"nonpayable","type":"function","contractId":{"contract":28595,"realm":0,"shard":0},"contractName":"HelloWorld"}],
+          params:[newMessage],
+          amount: 0
+      };
+      return await hash.triggerSmartContract(data);
+    },
+ 
+    message : async() => {
+      const data: ContractFunctionCallData= {
+          memo: "smart contract call",
+          contractid: "0.0.28595",
+          abi:[{"constant":true,"name":"message","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function","contractId":{"contract":28595,"realm":0,"shard":0},"contractName":"HelloWorld"}],
+          params:["afas"],
+          amount: 0
+      };
+      return await hash.triggerSmartContract(data);
     }
-
-   interface IFileRetrieveData {
-        fileId:string,
-        memo:string
-      }
-
- const file_hnstxt = {
+},
+ 
+ 
+ 
+filemyTestFiletxt : {
       fileRetrieve : async() => {
-          const data :IFileRetrieveData= {
+          const data: FileRetrieveData= {
               memo: "file retrieve",
-              fileId: "0.0.39692"
-          }
+              fileId: "0.0.28596"
+          };
           return await hash.triggerFileRetrieve(data);
       }
-    }
-interface ITopicUpdateData {
-        topicId:string,
-        memo?:string,
-        topicName?:string,
-        submitKeyList?:string|string[],
-        expirationTime?:string|number
-      }
-      interface ITopicInfoData {
-        topicId:string,
-        memo:string
-      }
-      interface ISubmitMessageData {
-        topicId:string,
-        memo:string,
-        message:string
-      }
-
-
- const topic_testing = {
-      topicUpdate : async(updatedData:ITopicUpdateData) => {
-          const data :ITopicUpdateData= {
-              topicId:"0.0.39693"
-          }
+    },
+ 
+ 
+topicjustATestTopic : {
+      topicUpdate : async(updatedData: TopicUpdateData) => {
+          const data: TopicUpdateData= {
+              topicId:"0.0.28597"
+          };
           if(updatedData.topicName){
               data.memo = updatedData.topicName;
           }
@@ -90,86 +119,34 @@ interface ITopicUpdateData {
           return await hash.triggerTopicUpdate(data);
       },
       
-
+ 
       topicInfo : async() => {
-          const data  :ITopicInfoData= {
+          const data: TopicInfoData= {
               memo: "consensus topic info",
-              topicId:"0.0.39693"
-          }
+              topicId:"0.0.28597"
+          };
           return await hash.triggerTopicInfo(data);
       },
       
-
+ 
       topicDelete : async() => {
-          const data :ITopicInfoData= {
+          const data: TopicInfoData= {
               memo: "consensus topic delete",
-              topicId:"0.0.39693"
-          }
+              topicId:"0.0.28597"
+          };
           return await hash.triggerTopicDelete(data);
       },
       
-
-      messageSubmit : async(message:string=" ") => {
-          const data :ISubmitMessageData= {
+ 
+      messageSubmit : async(message: string=" ") => {
+          const data: SubmitMessageData= {
               memo: "consensus message submit",
-              topicId:"0.0.39693",
+              topicId:"0.0.28597",
               message
-          }
+          };
           return await hash.triggerSubmitMessage(data);
       }
-    }
+    } ,
     
-
- const topic_2345 = {
-      topicUpdate : async(updatedData:ITopicUpdateData) => {
-          const data :ITopicUpdateData= {
-              topicId:"0.0.39695"
-          }
-          if(updatedData.topicName){
-              data.memo = updatedData.topicName;
-          }
-          if(updatedData.submitKeyList){
-            data.submitKeyList = updatedData.submitKeyList;
-          }
-          if(updatedData.expirationTime){
-            data.expirationTime = updatedData.expirationTime;
-          }
-          return await hash.triggerTopicUpdate(data);
-      },
-      
-
-      topicInfo : async() => {
-          const data  :ITopicInfoData= {
-              memo: "consensus topic info",
-              topicId:"0.0.39695"
-          }
-          return await hash.triggerTopicInfo(data);
-      },
-      
-
-      topicDelete : async() => {
-          const data :ITopicInfoData= {
-              memo: "consensus topic delete",
-              topicId:"0.0.39695"
-          }
-          return await hash.triggerTopicDelete(data);
-      },
-      
-
-      messageSubmit : async(message:string=" ") => {
-          const data :ISubmitMessageData= {
-              memo: "consensus message submit",
-              topicId:"0.0.39695",
-              message
-          }
-          return await hash.triggerSubmitMessage(data);
-      }
-    }
-
-export default {
-    init,
-    file_hnstxt,
-    topic_testing,
-    topic_2345,
-    hashSdk
-}
+    hashSdk : hash
+};
